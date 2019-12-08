7ba4ee9e49a879f69a5c86c86f3e8d3db09d5977
uriel-s <zaxuriel@gmail.com>
2019-12-08 17:00:17 +0200

    new complex function
    new complex function!
    func plus toString copy/////

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -1,149 +1,150 @@
 package Ex1;
 
+import java.util.concurrent.CompletionException;
+import java.util.function.Function;
+
 public class  ComplexFunction implements complex_function{
-	 Node root;
+	function root;
+	function right;
+	Operation Operation;
+
 
 	// constructors
 	public ComplexFunction() {
-		this.root= null;
-
+		this.root= this.right=null;
+		this.Operation=null;
 	}
 
-	public ComplexFunction(Operation op, Node l ,Node r)
+	public ComplexFunction(Operation op, function l ,function r)
 	{
 
-		this.root.setLeft(l); 
-		this.root.setRight(r); 
-		this.root.setOperation(op);
-		this.root.setFunction(null);
+		this.root=l; 
+		this.right=r; 
+		this.Operation =op;
+
 	}
 
 
+
+	
 
 	public ComplexFunction(function f) {
-		root= new Node(f);
+		this.root=f;
 
 	}
 
-	public ComplexFunction(Operation op, function f1,function f2)
-	{
 
-		Node left = new Node(f1); 
-		Node right = new Node(f2);
-		root = new Node(op,left,right);
-
-	}
 
 	// complecfunction funcs
 	//eror 
-	
+
 	public String toString () {
-		String s="";
-		s=s+this.root.getFunction().toString();
-		if(this.root.getLeft()!=null) s+=preOrder(this.root.getLeft());
-		if(this.root.getRight()!=null) s+=preOrder(this.root.getRight());
-
-
+		String s= "";
+if(this.root instanceof Monom ||this.root instanceof Polynom ) s+= root.toString();
+if (this.root instanceof ComplexFunction ) s+=root.toString();
+if(this.right instanceof Monom||this.right instanceof Polynom) s+= right.toString();
+if (this.right instanceof ComplexFunction ) s+=right.toString();	
+		
 		return s;
-
-
-	}
-
-
-
-	public String preOrder (Node n) {
-		String s="";
-		s=s+n.getFunction().toString();
-		if(n.getLeft()!=null)s=s+preOrder(n.getLeft()); 
-		if(n.getRight()!=null)s=s+preOrder(n.getRight()); 
-
-		return s;
-
-
-	}
-
-
-	@Override
-	public double f(double x) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
-	@Override
-	public function initFromString(String s) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-//didnt tested yet
-	@Override
-	public function copy() {
-
-		ComplexFunction cp = new ComplexFunction(); 
-		cp.root= new Node(this.root.getFunction());
-		cp.root.setLeft(this.root.getLeft());
-		cp.root.setRight(this.root.getRight());
-		cp.root.setOperation(this.root.getOperation());
-		return cp;
-	}
-
-	@Override
-	// didnt finished
-	public void plus(function f1) {
-		ComplexFunction temp = new ComplexFunction();
-		ComplexFunction cp = new ComplexFunction(Operation.Plus, this, f1);
-
-	}
-
-	@Override
-	public void mul(function f1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void div(function f1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void max(function f1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void min(function f1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void comp(function f1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public function left() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public function right() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Operation getOp() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-
+	
 }
+
+		//			String	s=inOrder(this);
+		//			return s;
+		//		}
+		//		public String inOrder (function f) {
+		//			String s="";
+		//			s=s+this.root.toString();
+		//			if(this.right!=null) s+=inOrder(this.right);
+		//			return s;
+		//		}
+		
+
+
+
+		@Override
+		public double f(double x) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+
+		@Override
+		public function initFromString(String s) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		//didnt tested yet
+		@Override
+
+		public function copy() {
+
+			ComplexFunction cp = new ComplexFunction(); 
+			cp.root=this.root.copy();
+			cp.Operation=this.Operation;
+			if(this.right!=null) cp.right=this.right.copy();	
+			return cp;
+		}
+
+		@Override
+		// didnt finished
+		public void plus(function f1) {
+			ComplexFunction f = (ComplexFunction) this.copy();
+			this.root=f;
+			this.right=f1;
+            this .Operation=Operation.Plus;
+
+		}
+
+		@Override
+		public void mul(function f1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void div(function f1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void max(function f1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void min(function f1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void comp(function f1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public function left() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public function right() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Operation getOp() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+
+
+	}
--- src/Ex1/maintets.java
+++ src/Ex1/maintets.java
@@ -7,16 +7,22 @@ public class maintets {
 
 		Monom m =new Monom("x");
 		Polynom p = new Polynom("2x^2");
-	ComplexFunction cp3 = new ComplexFunction(Operation.Plus, m , p);
-//		ComplexFunction cp2= new ComplexFunction(Operation.Plus, m , cp1);
-	
-	ComplexFunction cp1 = new ComplexFunction(p);
-	ComplexFunction cp2 = new ComplexFunction();
-	cp2= (ComplexFunction) cp1.copy();
-    cp1.root.setFunction(m);
-	  System.out.println(cp2);
-	  System.out.println(cp1);
-	  System.out.println(cp3);
+		ComplexFunction cp1 = new ComplexFunction(Operation.Plus, p , p);
+		ComplexFunction cp = new ComplexFunction(m);
+		cp1.plus(p);
+		//ComplexFunction cp1 = new ComplexFunction(p);
+		//ComplexFunction cp2 = new ComplexFunction(m);
+		//cp2= (ComplexFunction) cp1.copy();
+		//System.out.println(cp1);
+		//System.out.println(cp2);
+		//ystem.out.println(cp3);
+
+
+		System.out.println(cp1);
+		//	System.out.println(m);
+		//System.out.println(p);
+		//System.out.println(cp);
+
 
 	}
 
