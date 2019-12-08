c62a913eec95a350a47d2c060103f6c264c18c9c
YairAn <57504705+YairAn@users.noreply.github.com>
2019-12-08 18:01:59 +0200

    commit of yair
    add few tests

--- src/Ex1/Ex1Testing/PolynomTest.java
+++ src/Ex1/Ex1Testing/PolynomTest.java
@@ -5,25 +5,95 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import Ex1.Polynom;
-
+import Ex1.Monom;
 public class PolynomTest {
 
 	@Test
-	public void test1() {
-		Polynom expected =new Polynom();
-		Polynom expected2 =new Polynom();
-		assertEquals(expected, expected2);
+	public void testAddp() {
+		Polynom e1 = new Polynom("2x^3+7x");
+		Polynom e2 = new Polynom("x^3");
+		e1.add(e2);
+		Polynom e3 = new Polynom("3x^3+7x");
+		assertEquals(e1, e3);
 	
 	}
 	@Test
-	public void test() {
-		Polynom expected =new Polynom();
-		Polynom expected2 =new Polynom();
-		assertEquals(expected, expected2);
+	public void testAddm() {
+		Polynom e1 = new Polynom("2x^3+7x");
+		Monom e2 = new Monom("x^3");
+		e1.add(e2);
+		Polynom e3 = new Polynom("3x^3+7x");
+		assertEquals(e1, e3);
 	
 	}
-
+	@Test
+	public void testSubp() {
+		Polynom e1 = new Polynom("2x^3+7x");
+		Polynom e2 = new Polynom("x^3");
+		e1.substract(e2);
+		Polynom e3 = new Polynom("x^3+7x");
+		assertEquals(e1, e3);
 	
-	
-	
+	}
+	@Test
+	public void testMultp() {
+		Polynom e1 = new Polynom("2x^3+7x");
+		Polynom e2 = new Polynom("x^3");
+		e1.multiply(e2);
+		Polynom e3 = new Polynom("2x^6+7x^4");
+		assertEquals(e3,e1);
+	    
+	}
+	@Test
+	public void testArrange() {
+		Polynom e1 = new Polynom("7x+2x^3");	
+		Polynom e2 = new Polynom("x^3+x^3+7x");
+		assertEquals(e2,e1);    
+	}
+	@Test
+	public void testMultm() {
+		Polynom e1 = new Polynom("2x^3+7x+0");
+		Monom e2 = new Monom("x^3");
+		e1.multiply(e2);
+		Polynom e3 = new Polynom("2x^6+7x^4");
+		assertEquals(e3,e1);
+	    
+	}
+	@Test
+	public void testBuild() {
+		Polynom e1 = new Polynom("2x^3+7x+0");
+		String str =e1.toString();
+		String s ="+2.0x^3+7.0x^1";
+		boolean b=s.equals(str);
+		assertTrue(b);
+	    
+	}
+	@Test
+	public void testf() {
+		Polynom e1 = new Polynom("2x^3+7x");
+		double x=e1.f(3);
+		assertEquals(75, x, 0.0000001);
+	}
+	@Test
+	public void testIsZero() {
+		Polynom e1 = new Polynom("0x^3+7x+5");
+		Polynom e2 = new Polynom("0x^3+0x+5x-5x");
+        boolean b1 = e1.isZero();
+        boolean b2 = e2.isZero();
+        assertTrue(b2);
+        assertFalse(b1);
+	}
+	@Test
+	public void testderiviate() {
+		Polynom e1 = new Polynom("2x^3+7x+5");
+		Polynom e2 = new Polynom("6x^2+7");
+		e1=(Polynom)e1.derivative();
+		assertEquals(e2, e1);
+	}
+	@Test
+	public void testRoot() {
+		 Polynom c=new Polynom("x^2-3x");
+	        double ans=c.root(1, 4, 0.00000001);
+		assertEquals(, ans, 0.00000001);
+	}
 }
